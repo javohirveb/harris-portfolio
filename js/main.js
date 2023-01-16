8091d296e2af7405389a8bd9d1844a8f0b012add
javohirveb <javohirtoxtasinov0001@gmail.com>
2023-01-16 16:18:15 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+})
+
+// HIDDEN
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const section = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    section.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTap = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTap && scrollY <= sectionTap + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
